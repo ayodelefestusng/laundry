a663d89f52aa07553c2dab3ebdaf7bbf5450325f
Ayodele Adeyinka <ayodelefestusng@gmail.com>
2026-04-24 05:17:08 +0100

    Email changed to dmai

--- myproject/settings.py
+++ myproject/settings.py
@@ -305,23 +305,13 @@ SITE_URL = 'http://localhost:8000'
 #  Email Configuration
 # ==============================================================================
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
-# EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-# EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
-# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-# DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
-
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "mail.dignityconcept.tech"
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True   # because port 465 is SSL
-EMAIL_HOST_USER = "laundry@dignityconcept.tech"
-EMAIL_HOST_PASSWORD = "BUeu^vKv8@dH{&1U"   # your mailbox password
-DEFAULT_FROM_EMAIL = "Dignity Concept <laundry@dignityconcept.tech>"
-
-
+EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 
 
@@ -358,6 +348,9 @@ CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = TIME_ZONE
+
+
+
 # CELERY_BROKER_URL = "rediss://default:8824947e9537dcfd047d@147.182.194.8:6380/0?ssl_cert_reqs=CERT_NONE"
 # CELERY_RESULT_BACKEND = "rediss://default:8824947e9537dcfd047d@147.182.194.8:6380/0?ssl_cert_reqs=CERT_NONE"
 
