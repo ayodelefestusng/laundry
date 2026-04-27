0acd6329307e8b2c789627d7c9763c89f7562e56
Ayodele Adeyinka <ayodelefestusng@gmail.com>
2026-04-27 17:57:56 +0100

    Amended medial uRL

--- myproject/urls.py
+++ myproject/urls.py
@@ -7,6 +7,8 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from websockets import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('accounts/', include('django.contrib.auth.urls')),
@@ -18,3 +20,16 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path  
+# Add this to serve media in production
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
